9a0ea3e8d29894f472782865eced57613d3db3bf
Huang Chaohui <1466825121@qq.com>
2019-06-01 15:03:10 +0800

    "添加自己的适配器"

--- app/src/main/java/com/example/hui/myspinner/MainActivity.java
+++ app/src/main/java/com/example/hui/myspinner/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.hui.myspinner;
 
+import android.content.Context;
+import android.support.annotation.ArrayRes;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,11 +27,65 @@ public class MainActivity extends AppCompatActivity {
         第二个是一个int的资源，也就是我们下拉框要显示的数据源
         第三个还是一个int的资源，这里是我们刚才新建的布局资源
         */
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.datalist,R.layout.spinner_layout);
+        final ArrayAdapter adapter = MyAdapter.createFromResource(this,R.array.datalist,R.layout.spinner_layout);
         spinner.setBackgroundColor(0x0);
         //加载下拉框的样式
         adapter.setDropDownViewResource(R.layout.spinner_item);
+        //spinner.setPopupBackgroundResource(R.drawable.background);
+        spinner.setDropDownVerticalOffset(dip2px(30));
 
         spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ((MyAdapter) adapter).setPosition(i);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
+    private  int dip2px(float dpValue) {
+        final float scale = this.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+
+}
+
+class MyAdapter<T> extends ArrayAdapter<T>{
+
+    private int position;
+    public MyAdapter(@NonNull Context context, int resource, @NonNull T[] objects) {
+        super(context, resource, objects);
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = super.getDropDownView(position, convertView, parent);
+        TextView textView = (TextView)view;
+        if(this.position == position){
+            textView.setTextColor(0xff373741);
+            textView.getPaint().setFakeBoldText(true);
+        }
+        else{
+            textView.setTextColor(0xff6d6d6d);
+            textView.getPaint().setFakeBoldText(false);
+        }
+        return view;
+    }
+
+    public static @NonNull MyAdapter<CharSequence> createFromResource(@NonNull Context context,
+                                                                           @ArrayRes int textArrayResId, @LayoutRes int textViewResId) {
+        final CharSequence[] strings = context.getResources().getTextArray(textArrayResId);
+        return new MyAdapter<>(context, textViewResId, strings);
+    }
+
 }
